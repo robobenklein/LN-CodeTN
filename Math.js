334096fceb29a0797869283ae1d21c21f77586e9
s747447 <s747447@student.knoxschools.org>
2014-11-07 12:37:42 -0500

    Update Math.js

--- Math.js
+++ Math.js
@@ -1,22 +1,26 @@
 function math()
 {
 var kwh = document.getElementById('kwh').value;
-var pannelCount = document.getElementById('pannelCount').value;
+var panelCount = document.getElementById('panelCount').value;
 var budget = document.getElementById('budget').value;
 var payoff = document.getElementById('payoff').value;
 var averagePan;
 var kwhPerPan;
-var Feasibilty;
+var feasibilty;
 var savings;
-var pannelAmount;
-pannelAmount = pannelCount * 5;
-kwhPerPan = kwh * pannelAmount;
+var panelAmount;
+panelAmount = panelCount * 5;
+kwhPerPan = kwh * panelAmount;
 savings = kwhPerPan * 0.10;
-if (payoff * savings <= instilation)
+
+if (payoff * savings <= installation)
+
 {
-feasibility = "This will not be feasible for this ammount of time";
+feasibility = "This will not be feasible for this amount of time.";
 }
+
 else
+
 {
-feasibility = "This will be feasible for this ammount of time";
-}​
+feasibility = "This will be feasible for this ammount of time.";
+}
