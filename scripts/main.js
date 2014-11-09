2c7c425ef122c6761d4c1e1571c0bd55f8813bc8
robobenklein <robobenklein@gmail.com>
2014-11-09 17:40:06 -0500

    Working Map updates

--- scripts/main.js
+++ scripts/main.js
@@ -36,3 +36,24 @@ function getRoofSize(school) {
         return 10180;
     }
 }
+function getSchoolURL(school) {
+    if (school == "Amherst Elementary School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=5101+Schaad+Rd,+Tennessee+37931&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Farragut High School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=11237+Kingston+Pike,+Knoxville,+TN+37934&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Gibbs Elementary School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=7700+Tazewell+Pike,+TN+37721&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Gibbs High School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=7628+Tazewell+Pike,+Corryton,+TN+37721&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Hardin Valley Academy") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=11345+Hardin+Valley+Road+Knoxville+,+TN+37932&zoom=17&scale=1&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Karns Middle School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=2925+Gray+Hendrix+Rd,+Knoxville,+TN+379315&zoom=17&scale=1&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Karns High School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=2700+Byington+Solway+Rd,+Knoxville,+TN+37931&zoom=16&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "Powell High School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=2136+W+W+Emory+Rd,+Powell,+TN+37849&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    } else if (school == "West Valley Middle School") {
+        return "http://maps.googleapis.com/maps/api/staticmap?center=9118+George+Williams+Rd,+Knoxville,+TN+37922&zoom=17&scale=false&size=450x350&maptype=satellite&sensor=false&format=png&visual_refresh=true";
+    }
+}
